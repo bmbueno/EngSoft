d89bfcf048afaa32e80284149982f388297273cf
Bruno Morais <bruno.morais@inf.ufrgs.br>
2019-11-27 11:00:30 -0300

    Adiçao do CSS e altereações na nomeação de algumas variaveis

--- adicionaLinha.js
+++ adicionaLinha.js
@@ -1,12 +1,12 @@
 function adicionaLinha(idTabela) {
     var form = document.getElementById('formulario');
-    var professor = document.getElementById('professor');
+    var orientador = document.getElementById('orientador');
     var sala = document.getElementById('sala');
     var data = document.getElementById('data');
 
     var table = $('#example').DataTable();
     var rowNode = table
-        .row.add( [ professor.value, sala.value, data.value,"<button onclick='removeLinha(this)'>Cancelar reunião</button>"] )
+        .row.add( [ orientador.value, sala.value, data.value,"<button onclick='removeLinha(this)'>Cancelar reunião</button>"] )
         .draw()
         .node();
 
