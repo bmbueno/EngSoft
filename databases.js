d3cd5817b37946b5d22f80d5ba44ed1588caad04
Bruno Morais <bruno.morais@inf.ufrgs.br>
2019-11-28 20:13:41 -0300

    Ajustes na funcao de cancelamento de reunioes

--- databases.js
+++ databases.js
@@ -34,15 +34,11 @@ function mostrarReunioes(){
     
 }
 function cancelaReuniao(id) {
-
-    // $( "button.excluir" ).click(function() {
-    //   $(this).parent().parent().remove();
-    // });
     
     db.transaction(function(tx){
       tx.executeSql('DELETE FROM reunioes WHERE id=?',[id]);
   });
   
-    setTimeout(function() {location.reload();},10);
+    setTimeout(function() {location.reload();},100);
    
   }
